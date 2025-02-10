4eedbbe1932181f4e3b0ef62b0486c0478c15fbc
Nikolay <49346826+Pikanion@users.noreply.github.com>
2025-02-10 20:19:47 +0300

    Update Chrome Profile Manager.bat

--- Chrome Profile Manager.bat
+++ Chrome Profile Manager.bat
@@ -1,16 +1,14 @@
 @echo off
 chcp 65001 >nul
-title Chrome Profiles Autostart
+title Chrome Profiles Manager
 setlocal enabledelayedexpansion
 
 :: Пути
-:: chrome_exe - БЕЗ КАВЫЧЕК
-set chrome_exe=C:\Program Files\Google\Chrome\Application\chrome.exe
-:: user_data - С КАВЫЧКАМИ
-set user_data="C:\Users\$username$\AppData\Local\Google\Chrome\User Data"
+set chrome_exe="C:\Program Files\Google\Chrome\Application\chrome.exe"
+set user_data="C:\Users\schku\AppData\Local\Google\Chrome\User Data"
 
 :: Проверка существования Chrome
-if not exist "%chrome_exe%" (
+if not exist %chrome_exe% (
     echo Ошибка: Chrome не найден по указанному пути
     echo Путь: %chrome_exe%
     pause
@@ -25,34 +23,21 @@ if not exist %user_data% (
     exit /b 1
 )
 
-:: Получаем список профилей
-set i=0
-for /f "delims=" %%A in ('powershell -Command "Get-ChildItem -Path '%user_data%' -Filter 'Profile *' | Sort-Object { [int]($_.Name -replace '\D') } | ForEach-Object { $_.Name }"') do (
-    set /a i+=1
-    set "profile[!i!]=%%A"
-)
-
-:: Проверка наличия профилей
-if !i!==0 (
-    echo Ошибка: Профили не найдены
-    pause
-    exit /b 1
-)
-
-:: Возврат к стандартному интерфейсу CMD
-cmd.exe /c exit
-cls
+:: Загрузка списка профилей
+call :reload_profiles
 
 :menu
     echo.
     echo ==============================
-    echo ===      Главное меню      ===
+    echo ===     Главное меню       ===
     echo ==============================
     echo 1 - Посмотреть список профилей
-    echo 2 - Запуск профилей
-    echo 3 - Выход из программы
+    echo 2 - Создание профилей
+    echo 3 - Запуск профилей
+    echo 4 - Выход из программы
     echo ==============================
-    set /p "choice=Выберите опцию (1-3): "
+    echo.
+    set /p "choice=Выберите опцию (1-4): "
 
 if "%choice%"=="1" (
     cls
@@ -68,15 +53,44 @@ if "%choice%"=="1" (
     goto menu
 ) else if "%choice%"=="2" (
     cls
+    echo Создание новых профилей
+    set /p "num_profiles=Сколько профилей нужно создать: "
+    
+    :: Создаем новые профили
+    for /L %%n in (1,1,!num_profiles!) do (
+        :: Находим следующий доступный номер профиля
+        set /a next_profile=!max_profile_num!+%%n
+        set "new_profile_name=Profile !next_profile!"
+        echo.
+        echo Создание профиля №!next_profile!
+        start "" %chrome_exe% --profile-directory="!new_profile_name!" --no-startup-window
+        timeout /t 2 /nobreak >nul
+
+        :: Закрытие профиля (с проверкой наличия процесса)
+        powershell -Command "$processes = Get-Process chrome -ErrorAction SilentlyContinue; if ($processes) { $processes | Where-Object {$_.CommandLine -like '*--profile-directory=\"!new_profile_name!\"*'} | ForEach-Object { $_.CloseMainWindow() } }"
+        timeout /t 1 /nobreak >nul
+        echo Профиль №!next_profile! создан
+    )
+    echo.
+    echo Профили успешно созданы.
+    echo.
+    echo Выход в главное меню...
+    timeout /t 2 /nobreak >nul
+    cls
+    :: Автоматически обновляем список профилей после создания
+    call :reload_profiles
+    goto menu
+) else if "%choice%"=="3" (
+    cls
     echo.
     echo ================================
-    echo ===   2 - Запуск профилей    ===
+    echo ===   [3] Запуск профилей    ===
     echo ================================
     echo 1 - Запустить профиля выборочно
     echo 2 - Запустить диапазон профилей
     echo 3 - Вернуться в главное меню
     echo ================================
-
+    echo.
     set /p "sub_choice=Выберите опцию (1-3): "
 
     if "!sub_choice!"=="1" (
@@ -91,28 +105,32 @@ if "%choice%"=="1" (
         echo.
         set /p "profiles=Введите номера профилей через пробел (Например, 1 5 10): "
         echo.
-        set /p "urls=или Введите ссылки через пробел (Enter - пропустить): "
+        set /p "urls=Введите ссылки через пробел (Enter - пропустить): "
         echo.
+        cls
         echo Запуск выбранных профилей...
-
+        echo.
+        
         for %%a in (!profiles!) do (
-            echo Проверка профиля: %%a
             if %%a geq 1 if %%a leq !i! (
                 set "profile_path=!profile[%%a]!"
                 echo Запуск профиля: !profile_path!
-                timeout /t 1 /nobreak >nul
-                start "" "%chrome_exe%" --profile-directory="!profile_path!" !urls!
+                timeout /t 2 /nobreak >nul
+                start "" %chrome_exe% --profile-directory="!profile_path!" !urls!
             ) else (
                 echo Ошибка: Профиль %%a не существует
             )
         )
         echo.
+        echo Запуск профилей выполнен.
+        echo.
         pause
         cls
         goto menu
+
     ) else if "!sub_choice!"=="2" (
         cls
-        echo Выберите диапазон профилей для запуска
+        echo Выберите диапазон профилей для запуска.
         echo.
         echo Список профилей:
         echo.
@@ -123,6 +141,7 @@ if "%choice%"=="1" (
         set /p "ranges=Введите диапазоны профилей через запятую (Например, 1-3,5-7): "
         echo.
         set /p "urls=Введите ссылки через пробел (Enter - пропустить): "
+        
         :: Обработка каждого диапазона
         for %%r in (!ranges!) do (
             set "range=%%r"
@@ -131,13 +150,16 @@ if "%choice%"=="1" (
                 set "end=%%b"
                 
                 echo.
+                cls
                 echo Запуск профилей с !start! по !end!...
+                echo.
+                timeout /t 1 /nobreak >nul
                 for /L %%i in (!start!,1,!end!) do (
                     if %%i geq 1 if %%i leq !i! (
                         set "profile_path=!profile[%%i]!"
                         echo Запуск профиля: !profile_path!
-                        timeout /t 1 /nobreak >nul
-                        start "" "%chrome_exe%" --profile-directory="!profile_path!" !urls!
+                        timeout /t 2 /nobreak >nul
+                        start "" %chrome_exe% --profile-directory="!profile_path!" !urls!
                     ) else (
                         echo Ошибка: Профиль %%i не существует
                     )
@@ -145,9 +167,12 @@ if "%choice%"=="1" (
             )
         )
         echo.
+        echo Запуск профилей выполнен.
+        echo.
         pause
         cls
         goto menu
+
     ) else if "!sub_choice!"=="3" (
         cls
         goto menu
@@ -158,11 +183,11 @@ if "%choice%"=="1" (
         cls
         goto menu
     )
-) else if "%choice%"=="3" (
+) else if "%choice%"=="4" (
     echo.
     echo Выход из программы...
-    timeout /t 2 /nobreak >nul
-    exit /b 0
+    timeout /t 1 /nobreak >nul
+    exit
 ) else (
     echo.
     echo Неверный выбор, пожалуйста, попробуйте снова.
@@ -170,3 +195,21 @@ if "%choice%"=="1" (
     cls
     goto menu
 )
+
+:reload_profiles
+::echo Обновляем список профилей...
+:: Получаем список профилей и находим максимальный номер
+set i=0
+set max_profile_num=0
+for /f "delims=" %%A in ('powershell -Command "Get-ChildItem -Path '%user_data%' -Filter 'Profile *' | ForEach-Object { $_.Name -replace 'Profile ', '' } | Where-Object { $_ -match '^\d+$' } | Sort-Object {[int]$_} | Select-Object -Last 1"') do (
+    set "max_profile_num=%%A"
+)
+
+:: Получаем список профилей для отображения
+for /f "delims=" %%A in ('powershell -Command "Get-ChildItem -Path '%user_data%' -Filter 'Profile *' | Sort-Object { [int]($_.Name -replace '\D') } | ForEach-Object { $_.Name }"') do (
+    set /a i+=1
+    set "profile[!i!]=%%A"
+)
+::echo Список профилей обновлен.
+cls
+goto menu
